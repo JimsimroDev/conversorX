0dfa68fe29e14df64724938c637f55483ce41ae6
JHOAN R <jhoangd.jgd@gmail.com>
2024-04-12 21:43:55 -0500

    corrigiendo logica de conversion

--- src/com/jimsimrodev/conversorX/principal/TestConversor.java
+++ src/com/jimsimrodev/conversorX/principal/TestConversor.java
@@ -11,28 +11,31 @@ public class TestConversor {
     public static void main(String[] args) {
         StringBuilder menu = new  StringBuilder();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        ApiConversonMoneda conversorModeda = new ApiConversonMoneda();
         String opcion = "";
+        String USD = "USD";
+        String COP = "COP";
+        double cantidad = 0;
         menu.append(":::::::::::::::::::::CONVERSOR DE MONEDA:::::::::::::::::::::::::\n");
         menu.append("1 ~ de USD a COP\n");
         menu.append("2 ~ de COP a USD\n");
         menu.append("3 ~ salir\n");
         menu.append("¿Que desea hacer ?: ");
-        ApiConversonMoneda conversorModeda = new ApiConversonMoneda();
-       // System.out.println("Escoje la maneda a convertir");
         while (true) {
             try {
                 System.out.println(menu);
-                String USD = "USD";
-                String COP = "COP";
-                 opcion = in.readLine();
+                opcion = in.readLine();
+                System.out.println("Ingresa la cantidad que desea convertir");
+                cantidad = Double.parseDouble(in.readLine());
+
                 if ("1".equalsIgnoreCase(opcion)) {
                     Moneda monedaConvertida = conversorModeda.escojerMoneda(USD, COP);
                     //System.out.println(monedaConvertida.toString());
-                    System.out.println("EL TOTAL ES " + monedaConvertida.cantidaMonedas(5));
+                    System.out.println("EL TOTAL ES " + monedaConvertida.cantidaMonedas(cantidad));
                 }else if("2".equalsIgnoreCase(opcion)){
                     Moneda monedaConvertida = conversorModeda.escojerMoneda(COP, USD);
                     System.out.println(monedaConvertida.toString());
-                    System.out.println(monedaConvertida.cantidaMonedas(18893.794));
+                    System.out.println(monedaConvertida.cantidaMonedas(cantidad));
 
                 }
 
