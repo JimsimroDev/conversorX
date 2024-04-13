ba3a90d2efc45db498f8e7d1017d0b845df40707
JHOAN R <jhoangd.jgd@gmail.com>
2024-04-12 21:22:39 -0500

    convirtiendo valores

--- src/com/jimsimrodev/conversorX/Moneda.java
+++ src/com/jimsimrodev/conversorX/Moneda.java
@@ -3,10 +3,15 @@ package com.jimsimrodev.conversorX;
 public record Moneda(String base_code,
                      String target_code,
                      String conversion_rate) {
+
+    public String cantidaMonedas(double cantidad){
+        double total = cantidad * Double.parseDouble(conversion_rate);
+        return "El total es de " + total + target_code;
+    }
     @Override
     public String toString() {
         return  "Moneda a convertir= [" + base_code+ "]" +
                 ", Mondeda convertida a= [" + target_code +"]" +
-                ", Valore de la moneda es='" + conversion_rate + " " +target_code;
+                ", Valore de un = ["+ base_code +"] en [" + target_code + "]  es de [" +conversion_rate +"] "+target_code;
     }
 }
--- src/com/jimsimrodev/conversorX/principal/TestConversor.java
+++ src/com/jimsimrodev/conversorX/principal/TestConversor.java
@@ -6,23 +6,47 @@ import com.jimsimrodev.conversorX.modelo.ApiConversonMoneda;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.SQLOutput;
 
 public class TestConversor {
     public static void main(String[] args) {
+        StringBuilder menu = new  StringBuilder();
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
+        String opcion = "";
+        menu.append(":::::::::::::::::::::CONVERSOR DE MONEDA:::::::::::::::::::::::::\n");
+        menu.append("1 ~ de USD a COP\n");
+        menu.append("2 ~ de COP a USD\n");
+        menu.append("3 ~ salir\n");
+        menu.append("¿Que desea hacer ?: ");
         ApiConversonMoneda conversorModeda = new ApiConversonMoneda();
        // System.out.println("Escoje la maneda a convertir");
-        try {
-        String USD = "USD";
-        String cop = "COP";
-            Moneda monedaConvertida = conversorModeda.escojerMoneda(USD, cop);
-            System.out.println(monedaConvertida.toString());
-        }catch (NumberFormatException e){
-            System.out.println("No se encutra " + e.getMessage());
-        }catch (RuntimeException e){
-            System.out.println(e.getMessage());
-            System.out.println("Finalizacion la aplicacion");
+        while (true) {
+            try {
+                System.out.println(menu);
+                String USD = "USD";
+                String COP = "COP";
+                 opcion = in.readLine();
+                if ("1".equalsIgnoreCase(opcion)) {
+                    Moneda monedaConvertida = conversorModeda.escojerMoneda(USD, COP);
+                    //System.out.println(monedaConvertida.toString());
+                    System.out.println("EL TOTAL ES " + monedaConvertida.cantidaMonedas(5));
+                }else if("2".equalsIgnoreCase(opcion)){
+                    Moneda monedaConvertida = conversorModeda.escojerMoneda(COP, USD);
+                    System.out.println(monedaConvertida.toString());
+                    System.out.println(monedaConvertida.cantidaMonedas(18893.794));
+
+                }
+
+            } catch (IOException | NumberFormatException e) {
+                System.out.println("No se encutra " + e.getMessage());
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Finalizacion la aplicacion");
+            }
+            if("3".equalsIgnoreCase(opcion)){
+                break;
+            }
         }
+        System.out.println("Hasta pronto gracias por utilizar el programa...");
     }
 }
