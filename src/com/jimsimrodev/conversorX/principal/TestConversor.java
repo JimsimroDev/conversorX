301083981bc097bbaf991286d3a757eddc6e58e6
JHOAN R <jhoangd.jgd@gmail.com>
2024-04-14 15:58:26 -0500

    se crea un metodo para mostar el menu

--- src/com/jimsimrodev/conversorX/principal/TestConversor.java
+++ src/com/jimsimrodev/conversorX/principal/TestConversor.java
@@ -11,8 +11,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TestConversor {
-    public void run(){
+    private static final String DIVLINE = "+-------+-------+-------+-------+-------+-------+-------+";
+
+    public void titulo(){
+        String titulo = "CONVERSOR DE MONEDA";
         StringBuilder menu = new  StringBuilder();
+        menu.append("|   1   | dolar americano USD a peso colombiano COP     |\n");
+        menu.append("|   2   | peso colombiano COP a dolar americano USD     |\n");
+        menu.append("|   3   | peso boliviano BOB a Real brasileño BRL       |\n");
+        menu.append("|   4   | salir                                         |");
+
+        System.out.println("+-------------------------------------------------------+");
+        System.out.print("|");
+
+        int x = 1;
+        while (x < (DIVLINE.length() - titulo.length())/2){
+            System.out.print(" ");
+            x++;
+        }
+        System.out.print(titulo);
+        x = x + titulo.length();
+        while(x < (DIVLINE.length()-1)){
+            System.out.print(" ");
+            x++;
+        }
+        System.out.println("|");
+        System.out.println(DIVLINE);
+        System.out.print("|comando|");
+        System.out.println(" descripcion                                   |");
+        System.out.println(menu);
+        System.out.println(DIVLINE);
+        System.out.print("¿Que desea hacer ?: ");
+    }
+
+    public void run(){
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         ApiConversonMoneda conversorModeda = new ApiConversonMoneda();
         ConvertidorDeMoneda cant = new ConvertidorDeMoneda();
@@ -29,36 +61,33 @@ BOB - Boliviano boliviano
 BRL - Real brasileño
 CLP - Peso chileno
 COP - Peso colombiano
-USD - Dólar estadounidense
-        * */
+USD - Dólar estadounidense  * */
 
-        menu.append(":::::::::::::::::::::CONVERSOR DE MONEDA:::::::::::::::::::::::::\n");
-        menu.append("1 ~ de dolares americano USD a pesos colombianos COP\n");
-        menu.append("2 ~ de pesos colombianos COP a dolar americano USD\n");
-        menu.append("3 ~ de pesos boliviano BOB a Real brasileño BRL\n");
-        menu.append("4 ~ salir\n");
-        menu.append("¿Que desea hacer ?: ");
         while (true) {
+            titulo();
             try {
-                System.out.println(menu);
+                // System.out.println(menu);
                 opcion = in.readLine();
 
                 if ("1".equalsIgnoreCase(opcion)) {
                     cantidad = cant.realizarConversion(in);
                     Moneda monedaConvertida = conversorModeda.escojerMoneda(USD, COP);
-                    //System.out.println(monedaConvertida.toString());
-                    System.out.println("EL TOTAL ES " + monedaConvertida.cantidaMonedas(cantidad));
+                    System.out.println(monedaConvertida.toString());
+                    System.out.println(monedaConvertida.cantidaMonedas(cantidad));
+
                 }else if("2".equalsIgnoreCase(opcion)){
                     cantidad = cant.realizarConversion(in);
                     Moneda monedaConvertida = conversorModeda.escojerMoneda(COP, USD);
                     System.out.println(monedaConvertida.toString());
                     System.out.println(monedaConvertida.cantidaMonedas(cantidad));
+
                 }else if("3".equalsIgnoreCase(opcion)){
                     cantidad = cant.realizarConversion(in);
                     Moneda monedaConvertida = conversorModeda.escojerMoneda(BOB, BRL);
                     System.out.println(monedaConvertida.toString());
                     System.out.println(monedaConvertida.cantidaMonedas(cantidad));
                 }
+
             } catch (IOException | NumberFormatException e) {
                 System.out.println("No se encuentra esa moneda " + e.getMessage());
             } catch (RuntimeException e) {
